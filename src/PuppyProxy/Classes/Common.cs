f83e0ce366b8f9aa2620f25a854b3895a8b85bf5
Joel Christner <joel@maraudersoftware.com>
2022-08-15 13:09:33 -0700

    Restructure

---
+++ src/PuppyProxy/Classes/Common.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PuppyProxy
+{ 
+    internal static class Common
+    { 
+        internal static bool InputBoolean(string question, bool yesDefault)
+        {
+            Console.Write(question);
+
+            if (yesDefault) Console.Write(" [Y/n]? ");
+            else Console.Write(" [y/N]? ");
+
+            string userInput = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(userInput))
+            {
+                if (yesDefault) return true;
+                return false;
+            }
+
+            userInput = userInput.ToLower();
+
+            if (yesDefault)
+            {
+                if (
+                    (String.Compare(userInput, "n") == 0)
+                    || (String.Compare(userInput, "no") == 0)
+                   )
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                if (
+                    (String.Compare(userInput, "y") == 0)
+                    || (String.Compare(userInput, "yes") == 0)
+                   )
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+         
+        internal static string InputString(string question, string defaultAnswer, bool allowNull)
+        {
+            while (true)
+            {
+                Console.Write(question);
+
+                if (!String.IsNullOrEmpty(defaultAnswer))
+                {
+                    Console.Write(" [" + defaultAnswer + "]");
+                }
+
+                Console.Write(" ");
+
+                string userInput = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(userInput))
+                {
+                    if (!String.IsNullOrEmpty(defaultAnswer)) return defaultAnswer;
+                    if (allowNull) return null;
+                    else continue;
+                }
+
+                return userInput;
+            }
+        }
+         
+        internal static int InputInteger(string question, int defaultAnswer, bool positiveOnly, bool allowZero)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                Console.Write(" [" + defaultAnswer + "] ");
+
+                string userInput = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(userInput))
+                {
+                    return defaultAnswer;
+                }
+
+                int ret = 0;
+                if (!Int32.TryParse(userInput, out ret))
+                {
+                    Console.WriteLine("Please enter a valid integer.");
+                    continue;
+                }
+
+                if (ret == 0)
+                {
+                    if (allowZero)
+                    {
+                        return 0;
+                    }
+                }
+
+                if (ret < 0)
+                {
+                    if (positiveOnly)
+                    {
+                        Console.WriteLine("Please enter a value greater than zero.");
+                        continue;
+                    }
+                }
+
+                return ret;
+            }
+        }
+
+        internal static string SerializeJson(object obj, bool pretty)
+        {
+            if (obj == null) return null;
+            string json;
+
+            if (pretty)
+            {
+                json = JsonConvert.SerializeObject(
+                  obj,
+                  Newtonsoft.Json.Formatting.Indented,
+                  new JsonSerializerSettings
+                  {
+                      NullValueHandling = NullValueHandling.Ignore,
+                      DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                  });
+            }
+            else
+            {
+                json = JsonConvert.SerializeObject(obj,
+                  new JsonSerializerSettings
+                  {
+                      NullValueHandling = NullValueHandling.Ignore,
+                      DateTimeZoneHandling = DateTimeZoneHandling.Utc
+                  });
+            }
+
+            return json;
+        }
+
+        internal static T DeserializeJson<T>(string json)
+        {
+            if (String.IsNullOrEmpty(json)) throw new ArgumentNullException(nameof(json));
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        internal static T DeserializeJson<T>(byte[] data)
+        {
+            if (data == null || data.Length < 1) throw new ArgumentNullException(nameof(data));
+            return DeserializeJson<T>(Encoding.UTF8.GetString(data));
+        }
+
+        internal static T CopyObject<T>(object o)
+        {
+            if (o == null) return default(T);
+            string json = SerializeJson(o, false);
+            T ret = DeserializeJson<T>(json);
+            return ret;
+        }
+
+        internal static byte[] AppendBytes(byte[] orig, byte[] append)
+        {
+            if (append == null) return orig;
+            if (orig == null) return append;
+
+            byte[] ret = new byte[orig.Length + append.Length];
+            Buffer.BlockCopy(orig, 0, ret, 0, orig.Length);
+            Buffer.BlockCopy(append, 0, ret, orig.Length, append.Length);
+            return ret;
+        }
+    }
+}
---
+++ src/PuppyProxy/Classes/TunnelManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SyslogLogging;
+
+namespace PuppyProxy
+{    
+    internal class TunnelManager
+    {
+        #region Public-Members
+
+        #endregion
+
+        #region Private-Members
+
+        private LoggingModule _Logging;
+        private Dictionary<int, Tunnel> _Tunnels = new Dictionary<int, Tunnel>();
+        private readonly object _TunnelsLock = new object();
+
+        #endregion
+
+        #region Constructors-and-Factories
+
+        /// <summary>
+        /// Construct the tunnel manager.
+        /// </summary>
+        public TunnelManager()
+        {
+
+        }
+
+        /// <summary>
+        /// Construct the tunnel manager.
+        /// </summary>
+        /// <param name="logging">Logging module instance.</param>
+        public TunnelManager(LoggingModule logging)
+        {
+            _Logging = logging;  
+        }
+
+        #endregion
+
+        #region Internal-Methods
+         
+        internal void Add(int threadId, Tunnel curr)
+        {
+            lock (_TunnelsLock)
+            {
+                if (_Tunnels.ContainsKey(threadId)) _Tunnels.Remove(threadId);
+                _Tunnels.Add(threadId, curr);
+            }
+        }
+         
+        internal void Remove(int threadId)
+        {
+            lock (_TunnelsLock)
+            {
+                if (_Tunnels.ContainsKey(threadId))
+                {
+                    Tunnel curr = _Tunnels[threadId];
+                    _Tunnels.Remove(threadId);
+                    curr.Dispose();
+                }
+            }
+        }
+         
+        internal Dictionary<int, Tunnel> GetMetadata()
+        {
+            Dictionary<int, Tunnel> ret = new Dictionary<int, Tunnel>();
+
+            lock (_TunnelsLock)
+            {
+                foreach (KeyValuePair<int, Tunnel> curr in _Tunnels)
+                {
+                    ret.Add(curr.Key, curr.Value.Metadata());
+                }
+            }
+
+            return ret;
+        }
+         
+        internal Dictionary<int, Tunnel> GetFull()
+        {
+            Dictionary<int, Tunnel> ret = new Dictionary<int, Tunnel>();
+
+            lock (_TunnelsLock)
+            {
+                ret = new Dictionary<int, Tunnel>(_Tunnels);
+            }
+
+            return ret;
+        }
+         
+        internal bool Active(int threadId)
+        {
+            lock (_TunnelsLock)
+            {
+                if (_Tunnels.ContainsKey(threadId)) return true;
+            }
+
+            return false;
+        }
+         
+        internal int Count()
+        {
+            lock (_TunnelsLock)
+            {
+                return _Tunnels.Count;
+            }
+        }
+
+        #endregion
+
+        #region Private-Methods
+         
+        #endregion
+    }
+}
